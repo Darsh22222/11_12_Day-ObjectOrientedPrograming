c6528605ba8877fa7f4afe118a9cdbd61d3afe25
Darsh <darshankondhare2698@gmail.com>
2022-11-19 21:25:13 +0530

    [darsh]Add: To Display Welcome Message

---
+++ OOPS_PROBLEMS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPS_PROBLEMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to oops concept");
+        }
+    }
+}
