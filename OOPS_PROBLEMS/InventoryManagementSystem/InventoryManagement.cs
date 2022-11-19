e8b5c4bf20c6deec219563959af1eb7615453ddb
Darsh <darshankondhare2698@gmail.com>
2022-11-19 23:13:34 +0530

    [darsh] Add: Display Inventory Management Systems

---
+++ OOPS_PROBLEMS/InventoryManagementSystem/InventoryManagement.cs
@@ -0,0 +1,16 @@
+﻿using OOPS_PROBLEMS.InventoryManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_PROBLEMS.InventoryManagementSystem
+{
+    internal class InventoryManagement
+    {
+        public List<InventoryDetails>? RiceList { get; set; }
+        public List<InventoryDetails>? WheatList { get; set; }
+        public List<InventoryDetails>? PulsesList { get; set; }
+    }
+}
